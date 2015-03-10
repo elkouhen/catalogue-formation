8fa8d6b7a8e692f73244aa9323b527f6d9e6c42c
el <mehdi.elkouhen@gmail.com>
2015-03-10 22:53:22 +0100

    refactoring avec androidannotations

--- formationsIhmAndroid/app/src/main/java/formations/softeam/com/formationsihmandroid/services/FormationResource.java
+++ formationsIhmAndroid/app/src/main/java/formations/softeam/com/formationsihmandroid/services/FormationResource.java
@@ -2,14 +2,15 @@ package formations.softeam.com.formationsihmandroid.services;
 
 import org.androidannotations.annotations.rest.Get;
 import org.androidannotations.annotations.rest.Rest;
+import org.androidannotations.api.rest.RestClientErrorHandling;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 
-import java.util.Collection;
+import java.util.List;
 
-@Rest(rootUrl = "http://elkouhen-softeam-E6540:3000/formations", converters = {MappingJacksonHttpMessageConverter.class})
-public interface FormationResource {
+@Rest(rootUrl = "http://192.168.1.88:3000/formations", converters = {MappingJacksonHttpMessageConverter.class})
+public interface FormationResource extends RestClientErrorHandling {
 
     // OK
     @Get("")
-    Collection<Formation> findAll();
+    List<Formation> findAll();
 }
